cd238c1cda0cf8ee40f3c0992955490b1fb5da6d
charnelledev <kengnemaiva54@gmail.com>
2025-01-07 13:25:08 +0100

    JavaScript-1

---
+++ index.js
@@ -0,0 +1,50 @@
+// alert("le fichier fonctionne! ");
+// console.log("hello");
+
+// let variable = "KamelCase";
+
+// console.log(variable);
+
+// const prenom = "justine";
+// prenom = "justine";
+// console.log(prenom);
+
+
+// let string ="je suis une chaine de caractere";
+// let number = 24;
+// let boolean = true;
+// let array = ("je", "suis",24,false);
+// let object = {
+//     prenom: "audrey",
+//     age:34,
+//     ville:"bordeaux",
+// };
+
+// let arbre;
+
+// console.log(typeof arbre);
+
+
+// let x = 5;
+// let y = "5";
+// if (x === y || x > 3 && y < 10) {
+//     console.log("True !");
+// } else {
+//     console.log("false !");
+    
+// }
+let x = 5;
+let y = "5";
+if (x === y) {
+    console.log("True !");
+} else  if (x==y) {
+    console.log("x et y sont egaux en valeur");
+} else {
+    
+    console.log(x + " et "+ y " ne sont pas du tout egaux ");
+    console.log('$(x) et $(y) ne sont pas egaux');
+}
+let X = 6;
+let Y = "9";
+X == Y ? console.log("true !"):  console.log("false !");
+
